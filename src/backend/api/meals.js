a0fbef8ec6c10c840a1141bdd4d4c02d8700b5ed
slastushevskyi <slastushevskyi@gmail.com>
2023-03-25 08:34:16 +0100

    few small changes rebased

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -72,97 +72,105 @@ router.delete("/:id", async (req, res) => {
 
 router.get("/", async (req, res) => {
   try {
-    const data = await knex("Meal").select();
-    res.status(200).json(data);
-    if (!Object.keys(req.query).length) {
-      const data = await knex("Meal").select();
-      res.status(200).json(data);
-    } else {
-      const parameter = req.query;
-      for (const [key, value] of Object.entries(parameter)) {
-        if (key === "maxPrice") {
-          const price = parseInt(value);
-          const selectedMeals = await knex
-            .select()
-            .from("Meal")
-            .where("price", "<=", price);
-          res.json(selectedMeals);
-        } else if (key === "availableReservations") {
-          if (value === "true") {
-            const mealsWithRemainingReservations = knex
-              .select(
-                "Meal.*",
-                // I tried to avoid to use knex raw, but in this query it's unreal
-                knex.raw(
-                  // Coalesce is amazing sql fucntion that returs first NOT NULL value
-                  // In this case I have one meal that never been bought before and after join
-                  // tables it has NULL value. Coalesce gives me opportunity to use the value that
-                  // was before JOIN (maxreservation).
-                  "coalesce(Meal.maxreservation - sum(Reservation.number_of_guests), Meal.maxreservation) as remaining_reservation"
-                )
+    const parameter = req.query;
+    for (const [key, value] of Object.entries(parameter)) {
+      // Returns all meals that are cheaper than maxPrice.
+      if (key === "maxPrice") {
+        const price = parseInt(value);
+        const selectedMeals = await knex
+          .select()
+          .from("Meal")
+          .where("price", "<=", price);
+        res.json(selectedMeals);
+        // Returns all meals that still have available spots left, if true.
+      } else if (key === "availableReservations") {
+        if (value === "true") {
+          const mealsWithRemainingReservations = knex
+            .select(
+              "Meal.*",
+              // I tried to avoid to use knex raw, but in this query it's unreal
+              knex.raw(
+                // Coalesce is amazing sql fucntion that returs first NOT NULL value
+                // In this case I have one meal that never been bought before and after join
+                // tables it has NULL value. Coalesce gives me opportunity to use the value that
+                // was before JOIN (maxreservation).
+                "coalesce(Meal.maxreservation - sum(Reservation.number_of_guests), Meal.maxreservation) as remaining_reservation"
               )
-              .from("Meal")
-              .leftJoin("Reservation", "Reservation.meal_id", "Meal.id")
-              .groupBy("Meal.id")
-              .having(
-                knex.raw(
-                  "remaining_reservation > 0 OR remaining_reservation IS NULL"
-                )
-              );
-            res.status(200).json(await mealsWithRemainingReservations);
-          } else {
-            const mealsWithRemainingReservations = knex
-              .select(
-                "Meal.*",
-                knex.raw(
-                  "Meal.maxreservation - sum(Reservation.number_of_guests) as remaining_reservation"
-                )
+            )
+            .from("Meal")
+            .leftJoin("Reservation", "Reservation.meal_id", "Meal.id")
+            .groupBy("Meal.id")
+            .having(
+              knex.raw(
+                "remaining_reservation > 0 OR remaining_reservation IS NULL"
               )
-              .from("Meal")
-              .leftJoin("Reservation", "Reservation.meal_id", "Meal.id")
-              .groupBy("Meal.id")
-              .having("remaining_reservation", "<", 0);
-            res.json(await mealsWithRemainingReservations);
-          }
-        } else if (key === "title") {
-          const meals = await knex
-            .select()
-            .from("Meal")
-            .where("title", "like", `%${value}%`);
-          res.status(200).json(await meals);
-        } else if (key === "dateAfter") {
-          const meals = await knex
-            .select()
-            .from("Meal")
-            .where("when", ">", `${value}`);
-          res.status(200).json(await meals);
-        } else if (key === "dateBefore") {
-          const meals = await knex
-            .select()
-            .from("Meal")
-            .where("when", "<", `${value}`);
-          res.status(200).json(await meals);
-        } else if (key === "limit") {
-          const meals = await knex.select().from("Meal").limit(`${value}`);
-          res.status(200).json(await meals);
-        } else if (
-          req.query.sortKey === "when" ||
-          req.query.sortKey === "max_reservations" ||
-          req.query.sortKey === "price"
-        ) {
-          if (req.query.sortDir) {
-            const meals = await knex
-              .select()
-              .from("Meal")
-              .orderBy(`${req.query.sortKey}`, `${req.query.sortDir}`);
-            return res.status(200).json(await meals);
-          } else {
-            const meals = await knex.select().from("Meal").orderBy(`${value}`);
-            res.status(200).json(await meals);
-          }
+            );
+
+          res.status(200).json(await mealsWithRemainingReservations);
+          // If false, return meals that have no available spots left.
         } else {
-          res.status(404).json({ message: "Wrong query" });
+          const mealsWithRemainingReservations = knex
+            .select(
+              "Meal.*",
+              knex.raw(
+                "Meal.maxreservation - sum(Reservation.number_of_guests) as remaining_reservation"
+              )
+            )
+            .from("Meal")
+            .leftJoin("Reservation", "Reservation.meal_id", "Meal.id")
+            .groupBy("Meal.id")
+            .having("remaining_reservation", "<", 0);
+
+          res.json(await mealsWithRemainingReservations);
         }
+        // Returns all meals that partially match the given title.
+        // Rød grød will match the meal with the title Rød grød med fløde.
+      } else if (key === "title") {
+        const meals = await knex
+          .select()
+          .from("Meal")
+          .where("title", "like", `%${value}%`);
+        res.status(200).json(await meals);
+        // Returns all meals where the date for when is after the given date.
+      } else if (key === "dateAfter") {
+        const meals = await knex
+          .select()
+          .from("Meal")
+          .where("when", ">", `${value}`);
+        res.status(200).json(await meals);
+        // Returns all meals where the date for when is before the given date.
+      } else if (key === "dateBefore") {
+        const meals = await knex
+          .select()
+          .from("Meal")
+          .where("when", "<", `${value}`);
+        res.status(200).json(await meals);
+        // Returns the given number of meals.
+      } else if (key === "limit") {
+        const meals = await knex.select().from("Meal").limit(`${value}`);
+        res.status(200).json(await meals);
+        // Returns all meals sorted by the given key.
+        // Allows when, max_reservations and price as keys.
+        // Default sorting order is asc(ending).
+      } else if (
+        req.query.sortKey === "when" ||
+        req.query.sortKey === "max_reservations" ||
+        req.query.sortKey === "price"
+      ) {
+        // Returns all meals sorted in the given direction.
+        // Only works combined with the sortKey and allows asc or desc.
+        if (req.query.sortDir) {
+          const meals = await knex
+            .select()
+            .from("Meal")
+            .orderBy(`${req.query.sortKey}`, `${req.query.sortDir}`);
+          return res.status(200).json(await meals);
+        } else {
+          const meals = await knex.select().from("Meal").orderBy(`${value}`);
+          res.status(200).json(await meals);
+        }
+      } else {
+        res.status(404).json({ message: "Wrong query" });
       }
     }
   } catch (error) {
