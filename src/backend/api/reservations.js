ef3648cecfa6549e4806edd4ff76f4af7e4720ad
slastushevskyi <slastushevskyi@gmail.com>
2023-05-15 17:08:18 +0200

    quickfix

--- src/backend/api/reservations.js
+++ src/backend/api/reservations.js
@@ -6,7 +6,7 @@ const knex = require("../database");
 router.get("/", async (req, res) => {
   try {
     const data = await knex("Reservation").select();
-    res.status(200).json(await data);
+    res.status(200).json(data);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
